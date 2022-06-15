d7b685d825faf66f14d8a7822c0a2dc8619e96c3
chaimvaid <chaim@daneltech.com>
2022-06-15 17:31:38 +0300

    add decription and serivce

--- microkit/microkit.go
+++ microkit/microkit.go
@@ -4,6 +4,14 @@ import (
 	"io/ioutil"
 	"log"
 	"encoding/json"
+	"fmt"
+	"crypto/aes"
+    // "encoding/hex"
+	// "io"
+	// "crypto/rand"
+	"crypto/cipher"
+	"encoding/base64"
+	"bytes"
 )
 
 type MicroKit struct{
@@ -11,24 +19,86 @@ type MicroKit struct{
 	ConfigKit map[string]ConfigGroup
 }
 
+type ApiRes struct {
+	Config string
+}
+
 
 var kitInstance *MicroKit = nil
 
 func  InitKit(sdk_key string, options map[string]string) (*MicroKit) {
+	keySize := 24
 	if kitInstance == nil {
 		kitInstance = new(MicroKit)
 		client := new(HttpClient)
+		var secret string
+		sdk_key, secret = parseSdkKey(sdk_key, keySize, options[service])
+		
 		resp := client.Get(options["url"], sdk_key)
 		bodyBytes, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			log.Fatal(err)
 		}
-		var rawStructJson map[string]map[string]map[string]map[string]string
-		json.Unmarshal(bodyBytes, &rawStructJson)
+		
+		fmt.Printf("%s+++++%s",key, secret)
+		var data ApiRes
+		json.Unmarshal(bodyBytes, &data)
+		decryptBody, _ := decryptCBC(data.Config, secret)
+		
+		var rawStructJson map[string]map[string]map[string]string
+		json.Unmarshal(decryptBody, &rawStructJson)
 		// log.Fatalf("%v", rawStructJson)
-		kitInstance.ConfigKit = InitConfigKit(rawStructJson["config"])
+		kitInstance.ConfigKit = InitConfigKit(rawStructJson)
+		
 		kitInstance.KitReady = true
 	}
 	return kitInstance
 }
+
+func decryptCBC( content, textKey string) (plaintext []byte, err error) {
+    key, _ := base64.StdEncoding.DecodeString(textKey)
+    ciphertext, _ := base64.StdEncoding.DecodeString(content)
+	var block cipher.Block
+
+    if block, err = aes.NewCipher(key); err != nil {
+        return
+    }
+
+    if len(ciphertext) < aes.BlockSize {
+        fmt.Printf("ciphertext too short")
+        return
+    }
+
+    iv := ciphertext[:aes.BlockSize]
+    ciphertext = ciphertext[aes.BlockSize:]
+
+    cbc := cipher.NewCBCDecrypter(block, iv)
+    cbc.CryptBlocks(ciphertext, ciphertext)
+
+    plaintext = PKCS5Trimming(ciphertext)
+
+    return
+}
+
+func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func PKCS5Trimming(encrypt []byte) []byte {
+	padding := encrypt[len(encrypt)-1]
+	return encrypt[:len(encrypt)-int(padding)]
+}
+
+func parseSdkKey (key string, keySize int, service string) (string, string) {
+	var sdkKey string
+	if service != "" {
+		sdkKey = fmt.Sprintf("%s-%s", key[:len(key)-keySize], service)
+	} else {
+		sdkKey = key[:len(key)-(keySize+1)]
+	}
+	return sdkKey, key[len(key)-keySize:]
+}
+
 
--- microkit/microkit_test.go
+++ microkit/microkit_test.go
@@ -7,22 +7,22 @@ import (
 )
 
 func TestMicroKit(t *testing.T)  {
-	jsonv := `{"config":{"db":{"username":{"type": "string", "value": "test"}}}}`
+	jsonv := `{"config":"dc5+idVVGi00GpKlmkp5PyMHZ0AzpZuyl+0sfTVzYcFHCc\/8bMGvamSA3jfYEfJZJ6i6eMu4vHIvpqBF+\/LH4m9tO6Armcm4vSk9cM1rlrtypLA\/q+Ms1EUgXBWdoeJbZOz\/k8QvRu7VGz\/s9RtFPL3ouzMdKaFAj78hIMscQG5uv1ClZoLNXbLrAzO9sJPmyFS4GfUulgZ2\/sih3G7qNVaMclHcIVX6vtvz09pEF7CRH1SMZCXd\/ceh\/e1ifpeZk1g762TNF37NBIDFMf0f50dvpM0OAo+U8+WZtJASPriD4zPVvp5mGR+LdOrPYFFHpby+cyAqutWMo16a1qTIkPcQkt0XRcIGlANrCXjiCwZ8c+Vu2z3feLPurq1E1HTiTwPVB5zRuaVYstLqDmEkV3AZZI0IMAL\/nHKJFLGkFeWfVQNBlCwgeqrbSqLDh5Pn3m6\/N3MEiypSivKGjRsUSMsyIV2nrmPVUMvE3BMkXsTA0u0RYSMGCIQhqf1RbtypCfrnPIsJVA7+JSHfkMx3ZIA+1BPL9n+Hbvb4RQxPwXp+3XXkhCzKfdc2loWxMFCV\/NSi9XcGrFATLAYb9tz4\/aNhzXU+WWOLa3+RflLLSDHFKvz18pTOCNdmK4fujLv5mgH0KmNpWnndFRWY\/mJjuetBxov8NolSFrIXUcE\/MzqEc2IeB68Fv9AXMw1Uz62+Cq3rQVeTnTlattRNrtamWBluUMqA\/bZKIUP1LV\/gF8kSVSVzLWZ\/iHg1mr1YxYswkoVoKIpp3qBmkN6CyKtyHNmyRSxL\/d5fUfi5tUHmbZ\/1lMMtA0v3KC2up+uC245uup1V9H++6YHe392FaYyZe41C5A3V3vFMJiTC2VnSd\/ZbmjNLfwhPvUqcfwLApYwyXWjwPd2gNNM2lL+VVhsjTuCFNGZ+YccSKQXW4EJzUjg2YB2R\/MqFEYePL\/OyAv4K"}`
 	uri := "3"
 	port := 8083
 	MockServer(jsonv, port, uri)
-	microKit := InitKit("test_key", map[string]string{"url": fmt.Sprintf("http://localhost:%d/%s", port, uri)})
+	microKit := InitKit("3c20444b-6865-4dfb-bfee-0a0b7279572b-qPYBtP5lgv4f/TDOV17BZA==", map[string]string{"url": fmt.Sprintf("http://localhost:%d/%s", port, uri)})
 	if reflect.TypeOf(*microKit).String() != "microkit.MicroKit" {
 		t.Errorf("got %s want %s", reflect.TypeOf(*microKit), "microkit.MicroKit")
 	}
 }
 
 func TestMicroKitReady(t *testing.T)  {
-	jsonv := `{"config":{"db":{"username":{"type": "string", "value": "test"}}}}`
+	jsonv := `{"config":"dc5+idVVGi00GpKlmkp5PyMHZ0AzpZuyl+0sfTVzYcFHCc\/8bMGvamSA3jfYEfJZJ6i6eMu4vHIvpqBF+\/LH4m9tO6Armcm4vSk9cM1rlrtypLA\/q+Ms1EUgXBWdoeJbZOz\/k8QvRu7VGz\/s9RtFPL3ouzMdKaFAj78hIMscQG5uv1ClZoLNXbLrAzO9sJPmyFS4GfUulgZ2\/sih3G7qNVaMclHcIVX6vtvz09pEF7CRH1SMZCXd\/ceh\/e1ifpeZk1g762TNF37NBIDFMf0f50dvpM0OAo+U8+WZtJASPriD4zPVvp5mGR+LdOrPYFFHpby+cyAqutWMo16a1qTIkPcQkt0XRcIGlANrCXjiCwZ8c+Vu2z3feLPurq1E1HTiTwPVB5zRuaVYstLqDmEkV3AZZI0IMAL\/nHKJFLGkFeWfVQNBlCwgeqrbSqLDh5Pn3m6\/N3MEiypSivKGjRsUSMsyIV2nrmPVUMvE3BMkXsTA0u0RYSMGCIQhqf1RbtypCfrnPIsJVA7+JSHfkMx3ZIA+1BPL9n+Hbvb4RQxPwXp+3XXkhCzKfdc2loWxMFCV\/NSi9XcGrFATLAYb9tz4\/aNhzXU+WWOLa3+RflLLSDHFKvz18pTOCNdmK4fujLv5mgH0KmNpWnndFRWY\/mJjuetBxov8NolSFrIXUcE\/MzqEc2IeB68Fv9AXMw1Uz62+Cq3rQVeTnTlattRNrtamWBluUMqA\/bZKIUP1LV\/gF8kSVSVzLWZ\/iHg1mr1YxYswkoVoKIpp3qBmkN6CyKtyHNmyRSxL\/d5fUfi5tUHmbZ\/1lMMtA0v3KC2up+uC245uup1V9H++6YHe392FaYyZe41C5A3V3vFMJiTC2VnSd\/ZbmjNLfwhPvUqcfwLApYwyXWjwPd2gNNM2lL+VVhsjTuCFNGZ+YccSKQXW4EJzUjg2YB2R\/MqFEYePL\/OyAv4K"}`
 	uri := "4"
 	port := 8084
 	MockServer(jsonv, port, uri)
-	microKit := InitKit("test_key", map[string]string{"url": fmt.Sprintf("http://localhost:%d/%s", port, uri)})
+	microKit := InitKit("3c20444b-6865-4dfb-bfee-0a0b7279572b-qPYBtP5lgv4f/TDOV17BZA==", map[string]string{"url": fmt.Sprintf("http://localhost:%d/%s", port, uri)})
 	
 	if !microKit.KitReady  {
 		t.Errorf("got %t want %t", microKit.KitReady, true)
@@ -30,11 +30,11 @@ func TestMicroKitReady(t *testing.T)  {
 }
 
 func TestMicroKitConfigKitType(t *testing.T)  {
-	jsonv := `{"config":{"db":{"username":{"type": "string", "value": "test"}}}}`
+	jsonv := `{"config":"dc5+idVVGi00GpKlmkp5PyMHZ0AzpZuyl+0sfTVzYcFHCc\/8bMGvamSA3jfYEfJZJ6i6eMu4vHIvpqBF+\/LH4m9tO6Armcm4vSk9cM1rlrtypLA\/q+Ms1EUgXBWdoeJbZOz\/k8QvRu7VGz\/s9RtFPL3ouzMdKaFAj78hIMscQG5uv1ClZoLNXbLrAzO9sJPmyFS4GfUulgZ2\/sih3G7qNVaMclHcIVX6vtvz09pEF7CRH1SMZCXd\/ceh\/e1ifpeZk1g762TNF37NBIDFMf0f50dvpM0OAo+U8+WZtJASPriD4zPVvp5mGR+LdOrPYFFHpby+cyAqutWMo16a1qTIkPcQkt0XRcIGlANrCXjiCwZ8c+Vu2z3feLPurq1E1HTiTwPVB5zRuaVYstLqDmEkV3AZZI0IMAL\/nHKJFLGkFeWfVQNBlCwgeqrbSqLDh5Pn3m6\/N3MEiypSivKGjRsUSMsyIV2nrmPVUMvE3BMkXsTA0u0RYSMGCIQhqf1RbtypCfrnPIsJVA7+JSHfkMx3ZIA+1BPL9n+Hbvb4RQxPwXp+3XXkhCzKfdc2loWxMFCV\/NSi9XcGrFATLAYb9tz4\/aNhzXU+WWOLa3+RflLLSDHFKvz18pTOCNdmK4fujLv5mgH0KmNpWnndFRWY\/mJjuetBxov8NolSFrIXUcE\/MzqEc2IeB68Fv9AXMw1Uz62+Cq3rQVeTnTlattRNrtamWBluUMqA\/bZKIUP1LV\/gF8kSVSVzLWZ\/iHg1mr1YxYswkoVoKIpp3qBmkN6CyKtyHNmyRSxL\/d5fUfi5tUHmbZ\/1lMMtA0v3KC2up+uC245uup1V9H++6YHe392FaYyZe41C5A3V3vFMJiTC2VnSd\/ZbmjNLfwhPvUqcfwLApYwyXWjwPd2gNNM2lL+VVhsjTuCFNGZ+YccSKQXW4EJzUjg2YB2R\/MqFEYePL\/OyAv4K"}`
 	uri := "5"
 	port := 8085
 	MockServer(jsonv, port, uri)
-	microKit := InitKit("test_key", map[string]string{"url": fmt.Sprintf("http://localhost:%d/%s", port, uri)})
+	microKit := InitKit("3c20444b-6865-4dfb-bfee-0a0b7279572b-qPYBtP5lgv4f/TDOV17BZA==", map[string]string{"url": fmt.Sprintf("http://localhost:%d/%s", port, uri)})
 	
 	if reflect.TypeOf(microKit.ConfigKit).String() != "map[string]microkit.ConfigGroup" {
 		t.Errorf("got %s want %s", reflect.TypeOf(microKit.ConfigKit), "map[string]microkit.ConfigGroup")
@@ -42,11 +42,11 @@ func TestMicroKitConfigKitType(t *testing.T)  {
 }
 
 func TestMicroKitConfigKitValue(t *testing.T)  {
-	jsonv := `{"config":{"db":{"username":{"type": "string", "value": "test"}}}}`
+	jsonv := `{"config":"dc5+idVVGi00GpKlmkp5PyMHZ0AzpZuyl+0sfTVzYcFHCc\/8bMGvamSA3jfYEfJZJ6i6eMu4vHIvpqBF+\/LH4m9tO6Armcm4vSk9cM1rlrtypLA\/q+Ms1EUgXBWdoeJbZOz\/k8QvRu7VGz\/s9RtFPL3ouzMdKaFAj78hIMscQG5uv1ClZoLNXbLrAzO9sJPmyFS4GfUulgZ2\/sih3G7qNVaMclHcIVX6vtvz09pEF7CRH1SMZCXd\/ceh\/e1ifpeZk1g762TNF37NBIDFMf0f50dvpM0OAo+U8+WZtJASPriD4zPVvp5mGR+LdOrPYFFHpby+cyAqutWMo16a1qTIkPcQkt0XRcIGlANrCXjiCwZ8c+Vu2z3feLPurq1E1HTiTwPVB5zRuaVYstLqDmEkV3AZZI0IMAL\/nHKJFLGkFeWfVQNBlCwgeqrbSqLDh5Pn3m6\/N3MEiypSivKGjRsUSMsyIV2nrmPVUMvE3BMkXsTA0u0RYSMGCIQhqf1RbtypCfrnPIsJVA7+JSHfkMx3ZIA+1BPL9n+Hbvb4RQxPwXp+3XXkhCzKfdc2loWxMFCV\/NSi9XcGrFATLAYb9tz4\/aNhzXU+WWOLa3+RflLLSDHFKvz18pTOCNdmK4fujLv5mgH0KmNpWnndFRWY\/mJjuetBxov8NolSFrIXUcE\/MzqEc2IeB68Fv9AXMw1Uz62+Cq3rQVeTnTlattRNrtamWBluUMqA\/bZKIUP1LV\/gF8kSVSVzLWZ\/iHg1mr1YxYswkoVoKIpp3qBmkN6CyKtyHNmyRSxL\/d5fUfi5tUHmbZ\/1lMMtA0v3KC2up+uC245uup1V9H++6YHe392FaYyZe41C5A3V3vFMJiTC2VnSd\/ZbmjNLfwhPvUqcfwLApYwyXWjwPd2gNNM2lL+VVhsjTuCFNGZ+YccSKQXW4EJzUjg2YB2R\/MqFEYePL\/OyAv4K"}`
 	uri := "6"
 	port := 8086
 	MockServer(jsonv, port, uri)
-	microKit := InitKit("test_key", map[string]string{"url": fmt.Sprintf("http://localhost:%d/%s", port, uri)})
+	microKit := InitKit("3c20444b-6865-4dfb-bfee-0a0b7279572b-qPYBtP5lgv4f/TDOV17BZA==", map[string]string{"url": fmt.Sprintf("http://localhost:%d/%s", port, uri)})
 	
 	if reflect.TypeOf(microKit.ConfigKit["db"]).String() != "microkit.ConfigGroup" {
 		t.Errorf("got %s want %s", reflect.TypeOf(microKit.ConfigKit["db"]), "microkit.ConfigGroup")
